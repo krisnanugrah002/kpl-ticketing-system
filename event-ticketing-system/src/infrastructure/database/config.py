602837eb92db3a2b170dbfbd5a90061161f8bedc
LentifyX <valentinosinaga0810@gmail.com>
2026-06-04 07:33:18 +0700

    setup alembic and generate initial database migrations

--- event-ticketing-system/src/infrastructure/database/config.py
+++ event-ticketing-system/src/infrastructure/database/config.py
@@ -1,9 +1,10 @@
 import os
 from sqlalchemy import create_engine
 
+
 DATABASE_URL = os.environ.get(
     "DATABASE_URL", 
-    "postgresql://postgres:postgres@localhost:5432/event_ticketing_db"
+    "postgresql://postgres:akuvalen@localhost:5432/event_ticketing_db" #Ini disesuain sama Pass Postgress sendiri ya kris
 )
 
 engine = create_engine(DATABASE_URL, echo=True)
